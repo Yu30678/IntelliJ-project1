81848e75bfca00b6f4476ee4df64061196fce1b2
Yu30678 <mop753951@gmail.com>
2025-08-08 23:24:35 +0800

    update_cartdao3

--- src/main/java/dao/cartDAO.java
+++ src/main/java/dao/cartDAO.java
@@ -65,7 +65,7 @@ public class cartDAO {
     // 查看購物車
     public static List<cart> getCartByMemberId(int memberId) {
         List<cart> list = new ArrayList<>();
-        String sql = "SELECT * FROM cart ";
+        String sql = "SELECT * FROM cart WHERE member_id = ?";
         try (Connection conn = DBUtil.getConnection(); PreparedStatement stmt = conn.prepareStatement(sql)) {
             stmt.setInt(1, memberId);
             ResultSet rs = stmt.executeQuery();
